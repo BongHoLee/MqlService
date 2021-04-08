1c9d1744d8faf81ae6a517edc613c4a29abc8f48
leebongho <leebongho9204@gmail.com>
2021-04-08 11:24:49 +0900

    create kotin project

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    groovy
+    java
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "com.kcb"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    compile("org.codehaus.groovy:groovy-all:2.3.11")
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+
+    implementation ("com.github.jsqlparser:jsqlparser:4.0")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
