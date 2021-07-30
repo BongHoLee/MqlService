c2e11683d0e0235a36eea2e91b9d96da9a0a0271
leebongho <bh9204@naver.com>
2021-07-30 18:03:08 +0900

    orderby 구현 진행중

--- src/main/java/com/kcb/mqlService/mqlQueryDomain/mqlQueryClause/optionalClause/OrderByClause.java
+++ src/main/java/com/kcb/mqlService/mqlQueryDomain/mqlQueryClause/optionalClause/OrderByClause.java
@@ -3,29 +3,37 @@ package com.kcb.mqlService.mqlQueryDomain.mqlQueryClause.optionalClause;
 import com.kcb.mqlService.mqlQueryDomain.mqlData.MQLDataStorage;
 import com.kcb.mqlService.mqlQueryDomain.mqlExpression.MQLOperatingExpression;
 import com.kcb.mqlService.mqlQueryDomain.mqlExpression.element.MQLElement;
+import com.kcb.mqlService.mqlQueryDomain.mqlQueryClause.OptionalClause;
 
 import java.util.List;
 
-public class OrderByClause implements GeneralCondition {
+public class OrderByClause implements OptionalClause {
     private List<MQLElement> orderByElements;
+    private boolean asc;
 
     public OrderByClause(){}
 
-    public OrderByClause(MQLOperatingExpression expression) {
-        this.expression = expression;
+    public OrderByClause(List<MQLElement> orderByElements) {
+        this.orderByElements = orderByElements;
     }
 
-    public void setOperatingExpression(MQLOperatingExpression expression) {
-        this.expression = expression;
+    public void setOrderByElements(List<MQLElement> orderByElements) {
+        this.orderByElements = orderByElements;
+    }
+
+    public void setAsc(boolean asc) {
+        this.asc = asc;
     }
 
     @Override
     public MQLDataStorage executeClause(MQLDataStorage mqlDataStorage) {
+        // 1. element is COLUMN
+
+        // 2. element is SINGLE_ROW_FUNCTION
+
+        // 3. element is VALUE
         return expression.operatingWith(mqlDataStorage);
     }
 
-    @Override
-    public MQLOperatingExpression getOperatingExpression() {
-        return expression;
-    }
+
 }
